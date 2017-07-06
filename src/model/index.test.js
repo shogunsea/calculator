bf75c801701ca479a0148a493e2dabf250316e63
shenbomo <shenbomo@gmail.com>
2017-07-06 00:43:44 -0500

    Removed Mocha/Chai. Added Jest, Webpack. Changed index.test.js (#3)
    * Removed Mocha/Chai. Added Jest, Webpack. Changed index.test.js
    
    * fixed indentation after PR
    
    * added boilerplate code for testing

--- src/model/index.test.js
+++ src/model/index.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const {expect} = require('chai');
 const Calculator = require('./index');
 
 describe('Calculator', () => {
@@ -8,35 +7,35 @@ describe('Calculator', () => {
     this.instance = new Calculator();
   });
 
-  describe('instance attribtues', () => {
-    it('contains one current input with value 0', () => {
-      expect(this.instance.input).to.equal(0);
+  describe('instance attributes', () => {
+    test('contains one current input with value 0', () => {
+        expect(this.instance.input).toBe(0);
     });
 
-    it('contains two operands with value 0', () => {
-      expect(this.instance.valA).to.equal(0);
-      expect(this.instance.valB).to.equal(0);
+    test('contains two operands with value 0', () => {
+      expect(this.instance.valA).toBe(0);
+      expect(this.instance.valB).toBe(0);
     });
 
-    it('contains one result with value 0', () => {
-      expect(this.instance.result).to.equal(0);
+    test('contains one result with value 0', () => {
+      expect(this.instance.result).toBe(0);
     });
 
-    it('contains one operator, initially set to empty string', () => {
-      expect(this.instance.operator).to.equal('');
+    test('contains one operator, initially set to empty string', () => {
+      expect(this.instance.operator).toBe('');
     });
-  })
+  });
 
   describe('#receiveOperand', () => {
-    context('when receiving first input', () => {
+    describe('when receiving first input', () => {
       it('first operand will be assied with the input value', () => {
         this.instance.receiveOperand(3);
-        expect(this.instance.valA).to.equal(3);
+        expect(this.instance.valA).toBe(3);
       });
 
       it('second operand will remain value 0', () => {
         this.instance.receiveOperand(3);
-        expect(this.instance.valB).to.equal(0);
+        expect(this.instance.valB).toBe(0);
       });
 
     });
@@ -49,17 +48,21 @@ describe('Calculator', () => {
     // new input will starts from 0
     it('sets current input to 0');
 
-    context('when only first operand has value', () => {
+    describe('when only first operand has value', () => {
       it('does not call #evaluate');
     });
 
-    context('when both operands have value', () => {
+    describe('when both operands have value', () => {
       it('calls #evaluate');
-    })
+    });
 
   });
 
   describe('#add', () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(Calculator.prototype.add(1, 2)).toBe(3);
+    });
+
     it('sets the sum of operandA and operandB to result');
 
     it('sets the result to operandA');
@@ -67,6 +70,6 @@ describe('Calculator', () => {
     it('sets operandB to 0');
 
     it('calls #displayResult method');
-  })
+  });
 
 });
