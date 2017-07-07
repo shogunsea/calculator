6ff102a03877cf9db45464a1c03ca3f317cd440e
shogunsea <shogunsea08@gmail.com>
2017-07-07 13:50:30 -0700

    add assets task: copy css to public path

--- tasks/start.js
+++ tasks/start.js
@@ -17,6 +17,6 @@ gulp.task('start', 'Start hot reload server via nodemon.', function() {
     env: {'NODE_ENV': 'development'},
   }).on('start', function() {
     console.log(green(`Starting assets task...` ));
-    // execSync('gulp assets');
+    execSync('gulp assets');
   });
 });
