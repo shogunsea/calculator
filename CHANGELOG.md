f681e0321b438e8c5da38904d0209008133d9eb1
shogunsea <shogunsea08@gmail.com>
2017-08-04 17:39:17 -0500

    add html to public folder; add change log

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+# v1.0.0
+Initial release.
+- Basic UI.
+- Supports simple addition functionality.
--- tasks/assets.js
+++ tasks/assets.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const gulp = require('gulp');
-// const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const del = require('del');
 const runSequence = require('run-sequence');
-// const uglifycss = require('gulp-uglifycss');
 
-const assetsPathPattern = './src/assets/*.css';
+const htmlPathPattern = './src/*.html';
 const publicPath = './public';
 
 gulp.task('assets:clean', 'Clean up public folder', function() {
@@ -16,25 +14,19 @@ gulp.task('assets:clean', 'Clean up public folder', function() {
   ]);
 });
 
-// gulp.task('assets:sass', 'Compile sass into css', function() {
-gulp.task('assets:css', 'Copy css to public folder', function() {
-  return gulp.src(assetsPathPattern)
-    // .pipe(sass().on('error', sass.logError))
+gulp.task('assets:html', 'Copy html to public folder', function() {
+  return gulp.src(htmlPathPattern)
     .pipe(rename((path) => {
         path.dirname = path.dirname.replace('/frontend', '')
         .replace('modules/', '');
       }))
-    // .pipe(uglifycss({
-    //   'maxLineLen': 80,
-    //   'uglyComments': true,
-    // }))
     .pipe(gulp.dest('./public'));
 });
 
 gulp.task('assets', function() {
   return runSequence(
     'assets:clean',
-    'assets:css',
+    'assets:html',
     function(error) {
       if (error) {
         console.log(error.message);
