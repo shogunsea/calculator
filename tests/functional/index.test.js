55488bc93bee0db4836a2602b58a235c71feb524
shogunsea <shogunsea08@gmail.com>
2017-08-19 11:57:48 -0500

    use puppeteer instead of webdrivor io as api for headless chrome

--- tests/functional/index.test.js
+++ tests/functional/index.test.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import server from '../../src/server'; // starts app server
-import TestRunner from './testRunner'; // wraps actual tests within headless chrome
+// import TestRunner from './testRunner'; // wraps actual tests within headless chrome
+import PuppeteerLauncher from './puppeteer_launcher';
 
 describe('Integration tests', () => {
   afterAll(() => {
@@ -10,7 +11,8 @@ describe('Integration tests', () => {
   });
 
   test('all pass', async () => {
-    const result = await TestRunner.run();
+    // const result = await TestRunner.run();
+    const result = await PuppeteerLauncher.run();
     expect(result).toBe(true);
   });
 })
